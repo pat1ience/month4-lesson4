968174c3d156c85884ad56c87e0f57f11fc6aeba
Amangulov <amangulovazim12@gmail.com>
2025-04-05 04:31:49 +0600

    counter setInterval()

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,85 @@
-// MOVE BLOCK HW 1.2
+// 1) homeWork
+const gmailInput = document.querySelector("#gmail_input");
+const gmailButton = document.querySelector("#gmail_button");
+const gmailResult = document.querySelector("#gmail_result");
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+gmailButton.onclick = () => {
+  const resExp = /^[\D][\w]*@gmail.com$/;
 
-let positionX = 0
-let positionY = 0
+  if (resExp.test(gmailInput.value)) {
+    gmailResult.innerText = "OK";
+    gmailResult.style.color = "green";
+  } else {
+    gmailResult.innerText = "ERROR";
+    gmailResult.style.color = "red";
+  }
+};
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+// 2) homeWork
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
+
+let positionX = 0;
+let positionY = 0;
+
+const maxWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const maxHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
 const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
-}
+  if (positionX < maxWidth && positionY === 0) {
+    positionX++;
+    childBlock.style.left = `${positionX}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionX >= maxWidth && positionY < maxHeight) {
+    positionY++;
+    childBlock.style.top = `${positionY}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionY >= maxHeight && positionX >= 0) {
+    positionX--;
+    childBlock.style.left = `${positionX}px`;
+    requestAnimationFrame(moveBlock);
+  } else if (positionX <= 0 && positionY >= 0) {
+    positionY--;
+    childBlock.style.top = `${positionY}px`;
+    requestAnimationFrame(moveBlock);
+  } else {
+    requestAnimationFrame(moveBlock);
+  }
+};
 
 moveBlock();
+
+// 3) homeWork
+const counterElement = document.getElementById("seconds");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+let intervalId;
+let count = 0;
+
+function startCount() {
+  intervalId = setInterval(() => {
+    count++;
+    updateCount();
+  }, 1000);
+}
+
+function stopCount() {
+  clearInterval(intervalId);
+}
+
+function resetCount() {
+  stopCount()
+  count = 0
+  updateCount();
+}
+
+const updateCount = () => {
+  counterElement.innerText = count;
+};
+updateCount();
+
+startButton.addEventListener("click", startCount);
+stopButton.addEventListener("click", stopCount);
+resetButton.addEventListener("click", resetCount);
--- js/lesson.js
+++ js/lesson.js
@@ -1,17 +1,15 @@
-// PHONE CHECKER
-
-const phoneInput = document.querySelector('#phone_input')
-const phoneButton = document.querySelector('#phone_button')
-const phoneResult = document.querySelector('#phone_result')
-
-const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+const phoneInput = document.querySelector("#phone_input");
+const phoneButton = document.querySelector("#phone_button");
+const phoneResult = document.querySelector("#phone_result");
 
 phoneButton.onclick = () => {
-    if (regExp.test(phoneInput.value)) {
-        phoneResult.innerHTML = 'OK'
-        phoneResult.style.color = 'green'
-    } else {
-        phoneResult.innerHTML = 'NOT OK'
-        phoneResult.style.color = 'red'
-    }
-}
+  const resExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/;
+
+  if (resExp.test(phoneInput.value)) {
+    phoneResult.innerText = 'OK'
+    phoneResult.style.color = 'green'
+  } else { 
+    phoneResult.innerText = 'ERROR'
+    phoneResult.style.color = 'red'
+  }
+};
